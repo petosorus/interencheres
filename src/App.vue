f47bf4339feac68f40e3d9e00715dc6dc2bbcca2
petosorus <petosorus@hotmail.fr>
2020-07-26 22:44:30 +0200

    konami

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,42 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Recherche</router-link>
-      <router-link to="/newSale">Nouvelle vente</router-link>
-      <router-link to="/search">Recherche</router-link>
+    <div v-if="konamied">
+      <!-- <iframe src='https://gfycat.com/ifr/WhirlwindLameIbis?controls=0' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0;' allowfullscreen></iframe> -->
+      <iframe  src="https://www.youtube.com/embed/MS8OawQegYE?autoplay=1&controls=0&rel=0&showinfo=0" frameborder="0" allow="autoplay;" allowfullscreen></iframe>
     </div>
-    <router-view/>
+
+    <span  v-else>
+      <div id="nav">
+        <router-link to="/">Recherche</router-link>
+        <router-link to="/newSale">Nouvelle vente</router-link>
+        <router-link to="/search">Recherche</router-link>
+      </div>
+      <router-view/>
+    </span>
   </div>
 </template>
 
 <script>
 import NewSale from './components/NewSale.vue'
+import Konami from 'konami'
 
 export default {
-  name: 'App'  
+  name: 'App',
+  data() {
+    return {
+      konamied: false
+    }
+  },
+  methods: {
+    gameOverMate() {
+      this.konamied = true
+    }
+  },
+  mounted() {
+    new Konami(() => {
+      this.gameOverMate()
+    })
+  }
 }
 </script>
 <style>
@@ -36,5 +59,12 @@ export default {
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+iframe {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+
 }
 </style>
